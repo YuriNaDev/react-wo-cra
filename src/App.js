1a217a41b9cc8191f405e7f06482defde04dd600
YuriNa <nyryn0945@gmail.com>
2019-12-19 17:18:26 +0900

    eslint 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { hot } from 'react-hot-loader/root'
 import './style.scss'
 
 function App() {
-	return <div>App</div>
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		document.title = 'TEST APP'
+	}, [])
+
+	const onIncrease = () => {
+		setCount(count => count + 1)
+	}
+
+	const onDecrease = () => {
+		setCount(count => count - 1)
+	}
+
+	return (
+		<div>
+			<h1>{count}</h1>
+			<button onClick={onIncrease}>+1</button>
+			<button onClick={onDecrease}>-1</button>
+		</div>
+	)
 }
 
 export default hot(App)
